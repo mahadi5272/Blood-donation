80c9d767579192f35be41d7814ed7c5b1ccc5ba5
mahadi5272 <mahadihasan5272@gmail.com>
2025-12-21 11:08:26 +0600

    also functionality of TotalBloodDonet  addede

--- src/Pages/Dashbord/TotalBloodDonet.jsx
+++ src/Pages/Dashbord/TotalBloodDonet.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 const TotalBloodDonet = () => {
+      const axiosSecure = UseAxiosSecure();
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    axiosSecure.get("/all-requests")
+      .then(res => {
+        setRequests(res.data);
+      });
+  }, [axiosSecure]);
   return (
     <div className="p-6">
       <h2 className="text-2xl font-bold mb-4">
