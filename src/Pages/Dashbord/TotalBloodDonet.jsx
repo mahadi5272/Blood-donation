3223bce26d6edfab1c6ce9b0745ddcfcef43349b
mahadi5272 <mahadihasan5272@gmail.com>
2025-12-21 11:09:22 +0600

    also inputs add

--- src/Pages/Dashbord/TotalBloodDonet.jsx
+++ src/Pages/Dashbord/TotalBloodDonet.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import UseAxiosSecure from "../../hooks/UseAxiosSecure";
 
 const TotalBloodDonet = () => {
       const axiosSecure = UseAxiosSecure();
